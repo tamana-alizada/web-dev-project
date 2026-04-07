5f0c5f26495b75113507cc16f737feeb949f8ccc
Azizi Hashmatullah <h_azizi@kbtu.kz>
2026-04-08 00:19:49 +0500

    Add files via upload

---
+++ SweetSaver/src/app/sign-in/sign-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-sign-in',
+  imports: [RouterModule],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.css',
+})
+export class SignIn {
+
+}
---
+++ SweetSaver/src/app/sign-up/sign-up.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import{ RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [RouterModule],
+  templateUrl: './sign-up.html',
+  styleUrl: './sign-up.css',
+})
+export class SignUp {
+
+}
